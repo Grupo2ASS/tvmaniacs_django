39614ee7d4f11ddc955fcff5a01f75baeeccd812
javicabello <jacabell@uc.cl>
2013-10-10 14:37:38 -0400

    probando git en pycharm

--- extras/basic_db.py
+++ extras/basic_db.py
@@ -10,12 +10,15 @@ a1 = { "firstname" : "John", "lastname" : "Doe" }
 
 a2 = { "firstname" : "Kevin", "lastname" : "Bacon" }
 
+a3 = { "firstname" : "Walter", "lastname" : "White" }
+
 print "clearing"
 actors.remove()
 
 print "saving"
 actors.save(a1)
 actors.save(a2)
+actors.save(a3)
 
 print "searching"
 for a in actors.find():
