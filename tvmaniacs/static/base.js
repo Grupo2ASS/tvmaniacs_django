65601c7fffb350b7c666a677618d083e77cf1417
fjsandov <franciscosandoval15@gmail.com>
2013-11-14 20:54:22 -0300

    #1179159 order is preserved when performing searches or when sorting results of searches

--- tvmaniacs/static/base.js
+++ tvmaniacs/static/base.js
@@ -1,3 +1,41 @@
-$(function(){
+var QueryString = function () {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    	// If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    	// If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    	// If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  }
+    return query_string;
+} ();
 
+$(function() {
+    var check_all_exist = $('#order-by-name').length>0 && $('#order-by-user-rating').length>0;
+    check_all_exist = check_all_exist && $('#form-sort-hidden').length>0;
+    check_all_exist = check_all_exist && $('#series-search-form').length>0;
+
+    if(check_all_exist) {
+        $('#series-search-form').submit(function() {
+            var sort = (QueryString["sort"] == undefined) ? 'name' : QueryString["sort"];
+            $('#form-sort-hidden')[0].value = sort;
+            return true;
+        });
+
+        var query = QueryString["search_query"];
+        if(query != undefined) {
+            $('#order-by-name')[0].href += "&search_query="+query;
+            $('#order-by-user-rating')[0].href += "&search_query="+query;
+            $('#series_search_input')[0].value = query.replace('+',' ');
+        }
+    }
 });
