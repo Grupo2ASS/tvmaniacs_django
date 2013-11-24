bca55d552fcbbe6ceed8a0be00d7ca9642917229
javicabello <jacabell@uc.cl>
2013-11-24 17:31:57 -0300

    Se agrega en actores que cuando se encuentra un link que no es "ver mas", lo elimina y solo deja el contenido

--- tvmaniacs/models.py
+++ tvmaniacs/models.py
@@ -1,5 +1,6 @@
 from mongoengine import *
 from tvmaniacs_django.settings import DBNAME
+import re
 
 
 connect(DBNAME)
@@ -123,6 +124,15 @@ class Actors(Document):
 
     def get_bio(self):
         if self.bio:
+            #busca ocurrencias de links y entrega todos
+            match = re.findall(r'<a href="(.*?)".*>(.*)</a>', self.bio)
+            #si encuentra alguno
+            if match:
+                #para cada ocurrencia que encontro
+                for link, title in match:
+                    #si no es "See full bio" reemplaza el primer link solo por su contenido
+                    if title != "See full bio":
+                        self.bio = re.sub(r'<a href="(.*?)".*>(.*)</a>', title, self.bio, 1)
             return self.bio.replace('<a href="/', '<a href="http://www.imdb.com/')
         return ""
 
